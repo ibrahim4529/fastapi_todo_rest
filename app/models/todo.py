73f6a0877981ef2b70fe149ec3939c95b462e3f1
Ibrahim Hanif <ibrahim.hanif4529@gmail.com>
2022-02-09 16:22:31 +0700

    add reamde

--- app/models/todo.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import Field, SQLModel
-from typing import Optional
-
-class Todo(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str 
-    description: str 
-    is_done: bool = False
